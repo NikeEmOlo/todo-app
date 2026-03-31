cd4dace5f31bc3bdb05d33bd9abc79e00d9b36c7
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-03-31 11:58:35 +0100

    Develop the outline of a task class

---
+++ src/tasks.js
@@ -0,0 +1,28 @@
+class Task {
+    constructor(name, description, dueDate, project) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.project = project;
+    }
+
+    deleteTask() {
+        //logic to delete a task
+    }
+
+    expandTask() {
+        //logic to expand task and show more detailed view
+    }
+
+    editDueDate() {
+        //logic to edit the due date
+    }
+
+    editProject() {
+        //logic to edit the project it belongs to
+    }
+
+    editDescription() {
+        //logic to allow user to update the description
+    }
+}
